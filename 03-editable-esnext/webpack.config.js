de6552bb70d5fd1629e963f6b44ba31356088fd0
Elio Rivero <eliorivero@gmail.com>
2017-08-14 16:43:02 -0300

    Add editable example in esnext version (#6)

---
+++ 03-editable-esnext/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	entry: './block.js',
+	output: {
+		path: __dirname,
+		filename: 'block.build.js',
+	},
+	module: {
+		loaders: [
+			{
+				test: /.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+};
--- index.php
+++ index.php
@@ -16,6 +16,7 @@ include '01-basic/index.php';
 include '01-basic-esnext/index.php';
 include '02-stylesheets/index.php';
 include '03-editable/index.php';
+include '03-editable-esnext/index.php';
 include '04-controls/index.php';
 include '04-controls-esnext/index.php';
 include '05-recipe-card/index.php';
