35e0bf634c8823d1a1c1ae6ed4c1a49d3c6239e0
Ajit Bohra <ajit@lubus.in>
2018-11-07 00:20:42 +0530

    Add plugin manifest to each example (#47)
     Plugin manifest added to all examples for independent usage 
    
    Reference https://github.com/WordPress/gutenberg-examples/issues/36#issuecomment-416212869

--- 01-basic-esnext/index.php
+++ 01-basic-esnext/index.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Plugin Name: Gutenberg Examples Basic EsNext
+ * Plugin URI: https://github.com/WordPress/gutenberg-examples
+ * Description: This is a plugin demonstrating how to register new blocks for the Gutenberg editor.
+ * Version: 1.0.2
+ * Author: the Gutenberg Team
+ *
+ * @package gutenberg-examples
+ */
+
 defined( 'ABSPATH' ) || exit;
 
 /**
