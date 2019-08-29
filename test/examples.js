b54bc838ec7399997c5d5371ba76cee13e2fddc4
Grzegorz (Greg) Ziółkowski <grzegorz@gziolo.pl>
2019-08-29 09:28:07 +0200

    Try: Add very basic unit tests to validate it works with the repository at the global level (#84)

---
+++ test/examples.js
@@ -0,0 +1,28 @@
+/* global test, expect */
+/**
+ * External dependencies
+ */
+import { create as createTestRenderer } from 'react-test-renderer';
+
+/**
+ * WordPress dependencies
+ */
+import { Button } from '@wordpress/components';
+
+// @see https://jestjs.io/docs/en/getting-started
+test( 'adds 1 + 2 to equal 3', () => {
+	function sum( a, b ) {
+		return a + b;
+	}
+
+	expect( sum( 1, 2 ) ).toBe( 3 );
+} );
+
+// @see https://jestjs.io/docs/en/tutorial-react
+test( 'renders Button from WordPress components', () => {
+	const testRenderer = createTestRenderer(
+		<Button>Click Me!</Button>
+	);
+
+	expect( testRenderer.toJSON() ).toMatchSnapshot();
+} );
