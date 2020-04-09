65d653d5565619c49acd5304c73daacded8abbec
Grzegorz (Greg) Ziółkowski <grzegorz@gziolo.pl>
2020-04-09 11:14:59 +0200

    Chore: Update @wordpress/scripts dependency (#112)
    * Chore: Update @wordpress/scripts dependency
    
    * Fix vulnerabilities by running npm audit fix

--- 06-inner-blocks-esnext/build/index.asset.php
+++ 06-inner-blocks-esnext/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => 'b86b4e5e245543702371312a9cdcc483');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '5f1cae70faf55bb006c00a28a9d2e3d6');
