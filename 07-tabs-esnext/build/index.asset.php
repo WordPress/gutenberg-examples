e3b12c2ce52e62757e6ceef6b9c9ab422659b666
Jon Sakas <jon.sakas@gmail.com>
2020-05-27 14:01:19 -0600

    Add tabs example

---
+++ 07-tabs-esnext/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => 'd8112f16e58347572adfd8189bbe5c45');
