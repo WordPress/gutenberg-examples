ee5f1122e4592004e137c5fba8c69c9270622522
Ryan Welcher <ryan.welcher@automattic.com>
2021-09-23 14:10:38 -0400

    Add build files.

--- 01-basic-esnext/build/index.asset.php
+++ 01-basic-esnext/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '76443a43339fc3cbbfde323114738565');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd03b1c932e40e0ab9ea5054ddeec54c2');
