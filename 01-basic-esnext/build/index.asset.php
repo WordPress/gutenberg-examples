11238c44a4ce1e8ea90671e42ab8b23e030955c9
Marcus Kazmierczak <marcus@mkaz.com>
2021-10-08 06:29:27 -0700

    Remove polyfill dependencies (#155)
    With the merging of https://github.com/WordPress/gutenberg/pull/35038
    the wp-polyfill is no longer a required dependency for the blocks.

--- 01-basic-esnext/build/index.asset.php
+++ 01-basic-esnext/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd03b1c932e40e0ab9ea5054ddeec54c2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n' ), 'version' => 'd03b1c932e40e0ab9ea5054ddeec54c2');
