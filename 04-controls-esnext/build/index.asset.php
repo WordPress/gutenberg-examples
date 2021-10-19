0dbe78d7719deb671562f53a8c51e34a82f1dadb
Ryan Welcher <ryan.welcher@automattic.com>
2021-10-19 11:16:52 -0400

    Build the files.

--- 04-controls-esnext/build/index.asset.php
+++ 04-controls-esnext/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n' ), 'version' => '1308fa13597c9c273fa3727655254927');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => 'f010a6437cb423783a9d456fc88f7af5');
