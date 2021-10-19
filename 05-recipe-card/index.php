1f9e20cf3878d02589263344b7b6d23e62babbf7
Ryan Welcher <ryan.welcher@automattic.com>
2021-10-19 11:47:03 -0400

    Update block.

--- 05-recipe-card/index.php
+++ 05-recipe-card/index.php
@@ -33,25 +33,8 @@ function gutenberg_examples_05_register_block() {
 		// Gutenberg is not active.
 		return;
 	}
-
-	wp_register_script(
-		'gutenberg-examples-05',
-		plugins_url( 'block.js', __FILE__ ),
-		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor', 'underscore' ),
-		filemtime( plugin_dir_path( __FILE__ ) . 'block.js' )
-	);
-
-	wp_register_style(
-		'gutenberg-examples-05',
-		plugins_url( 'style.css', __FILE__ ),
-		array( ),
-		filemtime( plugin_dir_path( __FILE__ ) . 'style.css' )
-	);
-
-	register_block_type( 'gutenberg-examples/example-05-recipe-card', array(
-		'style' => 'gutenberg-examples-05',
-		'editor_script' => 'gutenberg-examples-05',
-	) );
+	// Register the block by passing the location of block.json to register_block_type.
+	register_block_type( __DIR__ );
 
   if ( function_exists( 'wp_set_script_translations' ) ) {
     /**
