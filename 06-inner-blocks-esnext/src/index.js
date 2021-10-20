4abe1a9129a3b701b90a498eb569918a9fc9cf22
Ryan Welcher <ryan.welcher@automattic.com>
2021-10-20 09:48:03 -0400

    Update Inner Blocks (ESNext) to use latest API (#163)
    * Update scripts package and add .eslintrc.
    
    * Update block.
    
    * Run build.

--- 06-inner-blocks-esnext/src/index.js
+++ 06-inner-blocks-esnext/src/index.js
@@ -1,21 +1,19 @@
+/**
+ * WordPress dependencies
+ */
 import { registerBlockType } from '@wordpress/blocks';
-import { InnerBlocks } from '@wordpress/block-editor';
 
-registerBlockType( 'gutenberg-examples/example-06-esnext', {
-	title: 'Example: Inner Blocks (ESNext)',
-	category: 'layout',
-	edit: ( { className } ) => {
-		return (
-			<div className={ className }>
-				<InnerBlocks />
-			</div>
-		);
-	},
-	save: ( { className } ) => {
-		return (
-			<div className={ className }>
-				<InnerBlocks.Content />
-			</div>
-		);
-	},
+/**
+ * Internal dependencies
+ */
+import json from '../block.json';
+import edit from './edit';
+import save from './save';
+
+const { name, ...settings } = json;
+
+registerBlockType( name, {
+	...settings,
+	edit,
+	save,
 } );
