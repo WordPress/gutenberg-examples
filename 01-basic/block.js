d069b51864416b2ff95ec17361ae49518bcc39ca
Ryan Welcher <ryan.welcher@automattic.com>
2021-10-21 15:46:51 -0400

    Update block to use apiVersion 2 and no longer override settings set on the server-side.

--- 01-basic/block.js
+++ 01-basic/block.js
@@ -6,9 +6,10 @@
  * Using inline styles - no external stylesheet needed.  Not recommended
  * because all of these styles will appear in `post_content`.
  */
-( function( blocks, i18n, element ) {
+( function( blocks, i18n, element, blockEditor ) {
 	var el = element.createElement;
 	var __ = i18n.__;
+	var useBlockProps = blockEditor.useBlockProps;
 
 	var blockStyle = {
 		backgroundColor: '#900',
@@ -17,23 +18,19 @@
 	};
 
 	blocks.registerBlockType( 'gutenberg-examples/example-01-basic', {
-		title: __( 'Example: Basic', 'gutenberg-examples' ),
-		icon: 'universal-access-alt',
-		category: 'layout',
-		example: {},
 		edit: function() {
 			return el(
 				'p',
-				{ style: blockStyle },
+				useBlockProps({ style: blockStyle }),
 				'Hello World, step 1 (from the editor).'
 			);
 		},
 		save: function() {
 			return el(
 				'p',
-				{ style: blockStyle },
+				useBlockProps.save({ style: blockStyle }),
 				'Hello World, step 1 (from the frontend).'
 			);
 		},
 	} );
-} )( window.wp.blocks, window.wp.i18n, window.wp.element );
+} )( window.wp.blocks, window.wp.i18n, window.wp.element, window.wp.blockEditor );
