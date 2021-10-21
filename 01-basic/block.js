266b79c0f940d8374f7d5616f7d94474a5629d21
Ryan Welcher <ryan.welcher@automattic.com>
2021-10-21 15:52:25 -0400

    Add eslint and update file.

--- 01-basic/block.js
+++ 01-basic/block.js
@@ -21,16 +21,20 @@
 		edit: function() {
 			return el(
 				'p',
-				useBlockProps({ style: blockStyle }),
-				'Hello World, step 1 (from the editor).'
+				useBlockProps( { style: blockStyle } ),
+				__( 'Hello World, step 1 (from the editor).',
+					'gutenberg-examples'
+				)
 			);
 		},
 		save: function() {
 			return el(
 				'p',
-				useBlockProps.save({ style: blockStyle }),
-				'Hello World, step 1 (from the frontend).'
+				useBlockProps.save( { style: blockStyle } ),
+				__( 'Hello World, step 1 (from the frontend).',
+					'gutenberg-examples'
+				)
 			);
 		},
 	} );
-} )( window.wp.blocks, window.wp.i18n, window.wp.element, window.wp.blockEditor );
+}( window.wp.blocks, window.wp.i18n, window.wp.element, window.wp.blockEditor ) );
