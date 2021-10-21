01259fbd96c640011fee8deedb04c415f4a785c2
Ryan Welcher <ryan.welcher@automattic.com>
2021-10-21 16:56:27 -0400

    Update Basic example to use apiVersion 2 (#168)
    * Update block to use apiVersion 2 and no longer override settings set on the server-side.
    
    * Add eslint and update file.

--- 01-basic/block.js
+++ 01-basic/block.js
@@ -6,9 +6,10 @@
  * Using inline styles - no external stylesheet needed.  Not recommended
  * because all of these styles will appear in `post_content`.
  */
-( function( blocks, i18n, element ) {
+( function( blocks, i18n, element, blockEditor ) {
 	var el = element.createElement;
 	var __ = i18n.__;
+	var useBlockProps = blockEditor.useBlockProps;
 
 	var blockStyle = {
 		backgroundColor: '#900',
@@ -17,23 +18,23 @@
 	};
 
 	blocks.registerBlockType( 'gutenberg-examples/example-01-basic', {
-		title: __( 'Example: Basic', 'gutenberg-examples' ),
-		icon: 'universal-access-alt',
-		category: 'layout',
-		example: {},
 		edit: function() {
 			return el(
 				'p',
-				{ style: blockStyle },
-				'Hello World, step 1 (from the editor).'
+				useBlockProps( { style: blockStyle } ),
+				__( 'Hello World, step 1 (from the editor).',
+					'gutenberg-examples'
+				)
 			);
 		},
 		save: function() {
 			return el(
 				'p',
-				{ style: blockStyle },
-				'Hello World, step 1 (from the frontend).'
+				useBlockProps.save( { style: blockStyle } ),
+				__( 'Hello World, step 1 (from the frontend).',
+					'gutenberg-examples'
+				)
 			);
 		},
 	} );
-} )( window.wp.blocks, window.wp.i18n, window.wp.element );
+}( window.wp.blocks, window.wp.i18n, window.wp.element, window.wp.blockEditor ) );
