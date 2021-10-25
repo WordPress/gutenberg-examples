2934466d374ed0bc4e4793c5e6aca8def8fd2bbc
Ryan Welcher <ryan.welcher@automattic.com>
2021-10-25 13:40:19 -0400

    Add tests for first 5 blocks.

---
+++ e2e-tests/02-stylesheets.spec.js
@@ -0,0 +1,24 @@
+/**
+ * WordPress dependencies
+ */
+import {
+	getEditedPostContent,
+	insertBlock,
+	createNewPost,
+} from '@wordpress/e2e-test-utils';
+
+/**
+ * Internal dependencies
+ */
+import json from '../02-stylesheets/block.json';
+const { title, name } = json;
+
+it( `Example: ${ title } block should be available`, async () => {
+	await createNewPost();
+	await insertBlock( title );
+
+	// Check if block was inserted
+	expect( await page.$( `[data-type="${ name }"]` ) ).not.toBeNull();
+
+	expect( await getEditedPostContent() ).toMatchSnapshot();
+} );
