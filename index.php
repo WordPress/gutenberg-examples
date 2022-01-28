6e94016bb39aef84c31f22e298f166aafd578745
Riad Benguella <benguella@gmail.com>
2022-01-28 10:46:32 +0100

    Add block supports tutorial

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ include '05-recipe-card-esnext/index.php';
 include '06-inner-blocks/index.php';
 include '06-inner-blocks-esnext/index.php';
 include '07-slotfills-esnext/index.php';
+include '08-block-supports-esnext/index.php';
 include 'format-api/index.php';
 include 'plugin-sidebar/plugin-sidebar.php';
 include 'meta-block/meta-block.php';
