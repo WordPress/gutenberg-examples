cd64cf66518888bf95f03e4636c1204c6c45a34b
Ryan Welcher <ryan.welcher@automattic.com>
2022-03-09 13:40:33 -0500

    Adds dynamic block example.

---
+++ 10-dynamic-block/src/edit.js
@@ -0,0 +1,46 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
+ */
+import { useBlockProps, RichText } from '@wordpress/block-editor';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @param  root0
+ * @param  root0.attributes
+ * @param  root0.attributes.message
+ * @param  root0.setAttributes
+ * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
+ * @return {WPElement} Element to render.
+ */
+export default function Edit( { attributes: { message }, setAttributes } ) {
+	return (
+		<RichText
+			{ ...useBlockProps() }
+			tagName="p"
+			value={ message }
+			onChange={ ( newMessage ) =>
+				setAttributes( { message: newMessage } )
+			}
+		/>
+	);
+}
